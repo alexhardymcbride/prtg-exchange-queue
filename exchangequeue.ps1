4ae35e44d960e036a8571f6e396d7c7dc1cec2a7
alexhardymcbride <35763945+alexhardymcbride@users.noreply.github.com>
2018-12-08 21:38:17 +0000

    Add files via upload

---
+++ exchangequeue.ps1
@@ -0,0 +1,19 @@
+﻿param ([string]$ExchangeServer)
+
+$ErrorActionPreference = "SilentlyContinue"
+$WarningActionPreference = "SilentlyContinue"
+
+$uri = "http://" + $ExchangeServer + "/PowerShell/"
+
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $uri
+$output = Import-PSSession $session -DisableNameChecking -AllowClobber
+$queues = Get-Queue
+
+$channels = @()
+
+foreach ($queue in $queues) {
+    $channel = @{"Channel" = $queue.NextHopDomain;"Value" = $queue.MessageCount;"Unit" = "Count";"LimitMode" = "1";"LimitMaxError" = "25"}
+    $channels += $channel
+}
+
+Write-Output (@{"prtg" = (@{"result" = $channels})} | ConvertTo-JSON -Depth 3)
